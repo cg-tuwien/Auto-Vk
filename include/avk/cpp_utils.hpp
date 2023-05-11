bd15995f25162e90a611951361bda00dabf766ff
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2023-05-11 17:26:35 +0200

    Added `div_ceil` to cpp_utils.hpp

--- include/avk/cpp_utils.hpp
+++ include/avk/cpp_utils.hpp
@@ -1127,5 +1127,51 @@ namespace avk
 		// Formula taken from here: https://nvpro-samples.github.io/vk_raytracing_tutorial_KHR/#shaderbindingtable
 		auto am1 = aAlignment - 1;
 		return (aAddress + am1) & ~am1;
-	};
+	}
+
+	/**
+	 *  Similar to std::common_type_t<A, B>, but if A or B are signed, the result will also be signed.
+	 *
+	 *  This differs from the regular type promotion rules, where signed types are promoted to unsigned types.
+	 *
+	 *  Source: https://stackoverflow.com/a/63436491/387023
+	 */
+	template <typename A, typename B>
+	using common_signed_t =
+		std::conditional_t<std::is_unsigned_v<A>&& std::is_unsigned_v<B>,
+		std::common_type_t<A, B>,
+		std::common_type_t<std::make_signed_t<A>, std::make_signed_t<B>>>;
+
+	/**
+	 *  \brief           Divides two integers and rounds the result up to the next integer.
+	 *  \tparam Dividend Dividend type
+	 *  \tparam Divisor  Divisor type
+	 *  \param x         Dividend value
+	 *  \param y         Divisor value
+	 *  \return          x / y, rounded up to the next integer.
+	 *
+	 *  Source: https://stackoverflow.com/a/63436491/387023
+	 */
+	template <typename Dividend, typename Divisor>
+	constexpr common_signed_t<Dividend, Divisor> div_ceil(Dividend x, Divisor y)
+	{
+		if constexpr (std::is_unsigned_v<Dividend> && std::is_unsigned_v<Divisor>) {
+			// quotient is always positive
+			return x / y + (x % y != 0);  // uint / uint
+		}
+		else if constexpr (std::is_signed_v<Dividend> && std::is_unsigned_v<Divisor>) {
+			auto sy = static_cast<std::make_signed_t<Divisor>>(y);
+			bool quotientPositive = x >= 0;
+			return x / sy + (x % sy != 0 && quotientPositive);  // int / uint
+		}
+		else if constexpr (std::is_unsigned_v<Dividend> && std::is_signed_v<Divisor>) {
+			auto sx = static_cast<std::make_signed_t<Dividend>>(x);
+			bool quotientPositive = y >= 0;
+			return sx / y + (sx % y != 0 && quotientPositive);  // uint / int
+		}
+		else {
+			bool quotientPositive = (y >= 0) == (x >= 0);
+			return x / y + (x % y != 0 && quotientPositive);  // int / int
+		}
+	}
 }
